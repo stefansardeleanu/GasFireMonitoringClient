bba76b8c2a416e5486c1de98cf6d524df7cf13f1
stefansardeleanu <ardeleanu.s.stefan@gmail.com>
2025-08-15 09:54:41 +0300

    2.36FixedDB_Con

--- obj/Release/net9.0-windows/GasFireMonitoringClient.AssemblyInfo.cs
+++ obj/Release/net9.0-windows/GasFireMonitoringClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GasFireMonitoringClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2babf3d6ab6a47c31be1d961b4e9eb28990fafb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99e8488130fd0dda98ead497ea901629013c892b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GasFireMonitoringClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GasFireMonitoringClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
